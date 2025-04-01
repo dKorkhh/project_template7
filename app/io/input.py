1511273ab44c0532c83f19fff015029053180ec8
Dana Mushchenko <danamushchenko@gmail.com>
2025-04-01 17:42:40 +0300

    Add empty functions for input and output

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def read_console():
+    pass
+
+def read_file(filepath):
+    pass
+
+def read_file_pandas(filepath):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def write_console(text):
+    pass
+
+def write_file(filepath, text):
+    pass
